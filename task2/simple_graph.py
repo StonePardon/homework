4fe11d37abf8a073fa1ede445daf880748e51af5
StonePardon <55671818+StonePardon@users.noreply.github.com>
2019-09-29 22:21:05 +0300

    Update simple_graph.py

--- task2/simple_graph.py
+++ task2/simple_graph.py
@@ -1,37 +1,35 @@
 from collections import deque
 
 
-def make_graph(ribs):
-    graph = {}
-    for pair in ribs:
-        if pair[0] not in graph:
-            graph[pair[0]] = []
-        graph[pair[0]].append(pair[1])
-        if pair[1] not in graph:
-            graph[pair[1]] = []
-        graph[pair[1]].append(pair[0])
-    return graph
+class Graph:
+    def __init__(self, x):
+        self.array = {}
+        self.ribs = x
+        for pair in x:
+            if pair[0] not in self.array:
+                self.array[pair[0]] = []
+            self.array[pair[0]].append(pair[1])
+            if pair[1] not in self.array:
+                self.array[pair[1]] = []
+            self.array[pair[1]].append(pair[0])
 
+    def dfs(self, start):
+        visited = []
+        stack = [start]
+        while stack:
+            node = stack.pop()
+            if node not in visited:
+                visited.append(node)
+                stack.extend(set(self.array[node]) - set(visited))
+        return visited
 
-def dfs(graph, start):
-    visited = []
-    stack = [start]
-    while stack:
-        node = stack.pop()
-        if node not in visited:
-            visited.append(node)
-            stack.extend(set(graph[node]) - set(visited))
-    return visited
-
-
-def bfs(graph, start):
-    visited = []
-    list_of_node = deque([start])
-    while list_of_node:
-        node = list_of_node.pop()
-        if node not in visited:
-            visited.append(node)
-            list_of_node.extendleft(set(graph[node]) - set(visited))
-    return visited
-
+    def bfs(self, start):
+        visited = []
+        list_of_node = deque([start])
+        while list_of_node:
+            node = list_of_node.pop()
+            if node not in visited:
+                visited.append(node)
+                list_of_node.extendleft(set(self.array[node]) - set(visited))
+        return visited
 
