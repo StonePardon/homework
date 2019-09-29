0c9ac2de2416dd0f6fdc8114558ebd7d29bcf099
StonePardon <55671818+StonePardon@users.noreply.github.com>
2019-09-29 18:02:25 +0300

    Add files via upload

---
+++ task2/phone_number.py
@@ -0,0 +1,24 @@
+PHONE_BUTTONS = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+}
+
+
+def make_path(input_str, path, ans):
+    if input_str == '':
+        ans.append(path)
+        return
+    for let in PHONE_BUTTONS[input_str[0]]:
+        make_path(input_str[1:], path + let, ans)
+
+
+def phone_number(number):
+    ans = []
+    make_path(number, '', ans)
+    return ans
